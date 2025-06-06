5f910bc80996274ce899e5ea36c8c66189bf7c4b
geyajalui <jaluigeya24@gmail.com>
2025-06-06 14:50:04 +0530

    Update min_max.java

--- day_1/min_max.java
+++ day_1/min_max.java
@@ -5,10 +5,12 @@ public class min_max { public static void main(String[] args) {
     int max = numbers[0];
     int min = numbers[0];
     for (int i = 1; i < numbers.length; i++) {
-        if (numbers[i] > max) {
+        if (numbers[i] > max) 
+        {
             max = numbers[i];
         }
-        if (numbers[i] < min) {
+        if (numbers[i] < min) 
+        {
             min = numbers[i];
         }
     }
