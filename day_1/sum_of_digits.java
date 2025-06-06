761561b24ca276177687d22941eb6f180beb3232
geyajalui <jaluigeya24@gmail.com>
2025-06-06 14:30:47 +0530

    Update sum_of_digits.java

--- day_1/sum_of_digits.java
+++ day_1/sum_of_digits.java
@@ -1,5 +1,27 @@
 package day_1;
+import java.util.*;
 
-public class sum_of_digits {
+public class SumOfDigits {
+	public static int sumOfDigits(int number) {
+        if (number == 0) {
+            return 0;
+        }
+        return (number % 10) + sumOfDigits(number / 10);
+    }
 
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int input = scanner.nextInt();
+
+        int result = sumOfDigits(input);
+        System.out.println("Sum of digits: " + result);
+
+        scanner.close();
+    }
 }
+
+
+
+
